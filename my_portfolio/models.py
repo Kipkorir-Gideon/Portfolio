db9f4e4980b190600fb21d6038376204910eb714
Kipkorir-Gideon <gideon.ngetich@student.moringaschool.com>
2022-01-05 12:29:08 +0300

    Add a projects model.

--- my_portfolio/models.py
+++ my_portfolio/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from cloudinary.models import CloudinaryField
 
 # Create your models here.
+class Projects(models.Model):
+    title = models.CharField(max_length=100)
+    screenshot = CloudinaryField('image')
+    description = models.TextField()
+    languages = models.CharField(max_length=255)
+    site_url = models.URLField()
