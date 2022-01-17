7c410301d935ac76386b682c45c62556ce3a5631
Kipkorir-Gideon <gideon.ngetich@student.moringaschool.com>
2022-01-17 16:13:00 +0300

    Make the resume viewable and downloadable.

--- my_portfolio/views.py
+++ my_portfolio/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import *
+import os
 
 # Create your views here.
 def home(request):
--- portfolio/urls.py
+++ portfolio/urls.py
@@ -16,9 +16,16 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import include
+from django.contrib.staticfiles.urls import static, staticfiles_urlpatterns
+
+from . import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('my_portfolio.urls')),
     path('tinymce', include('tinymce.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += staticfiles_urlpatterns()
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
