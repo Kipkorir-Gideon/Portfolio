a90c3d31887b54ddeca54d072de9e3f6905aee6f
Kipkorir-Gideon <gideon.ngetich@student.moringaschool.com>
2022-01-23 22:56:44 +0300

    Create Languages model.

--- my_portfolio/models.py
+++ my_portfolio/models.py
@@ -2,9 +2,20 @@ from django.db import models
 from cloudinary.models import CloudinaryField
 
 # Create your models here.
+class Languages(models.Model):
+    name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
+
+
+
 class Projects(models.Model):
     title = models.CharField(max_length=100)
     screenshot = CloudinaryField('image')
     description = models.TextField()
-    languages = models.CharField(max_length=255)
+    languages = models.ManyToManyField(Languages)
     site_url = models.URLField()
+
+    def __str__(self):
+        return self.title
